93fc2c51534cd287eb15688febedc3200426a4f0
Jake McMillan <jakewmcmillan@gmail.com>
2022-09-16 14:45:59 -0400

    Implementing asset cachcing

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -22,10 +22,7 @@ export const putDb = async (content) => {
 
   const store = tx.objectStore('jate');
 
-  const request = store.put({
-    id: 1,
-    value: content
-  });
+  const request = store.put({ id: 1, content });
 
   const result = await request;
   console.log('result.value', result);
